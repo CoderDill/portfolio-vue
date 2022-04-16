b28973ca0e5b01e3d7bc375e20f9425eddac9534
CrappyDill <mddillon47@gmail.com>
2022-04-15 18:49:11 -0700

    tailwind container class center

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,12 +4,13 @@
   >
     <div class="container flex flex-wrap justify-between items-center mx-auto">
       <a href="https://www.coderdill.com" class="flex items-center">
-        <img src="/favicon.ico" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <!-- <img src="/favicon.ico" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
         <span
           class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
           >Matthew Dillon</span
         >
       </a>
+
       <button
         data-collapse-toggle="mobile-menu"
         type="button"
