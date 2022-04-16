ede2b39f41ae163decaba7005ff1b5a88047cbcb
CrappyDill <mddillon47@gmail.com>
2022-04-15 17:33:49 -0700

    Work Exp start

--- src/views/WorkExpView.vue
+++ src/views/WorkExpView.vue
@@ -1,10 +1,17 @@
 <template>
-    <h1>Work Experience</h1>
+  <h1 class="text-2xl text-center underline font-bold">
+    Work Experience
+  </h1>
+  <div class="font-bold m-3">
+    <img
+      src="public/launchBadgeLogo.png"
+      class="mr-1 h-6 sm:h-9 rounded-full inline-block"
+      alt=""
+    />- LaunchBadge LLC
+    <p>All the things related to LaunchBadge</p>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style>
-</style>
+<style></style>
